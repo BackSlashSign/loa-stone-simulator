bbc7a8121ccdbaa3ec0d64e0fd44a9cf61b74e66
파도새 <jsm9943@naver.com>
2023-06-06 18:08:11 +0900

    added merging
    .

--- Assets/2.Scripts/2048_2/TileBoard.cs
+++ Assets/2.Scripts/2048_2/TileBoard.cs
@@ -9,6 +9,9 @@ public class TileBoard : MonoBehaviour
 
     private TileGrid grid;
     private List<Tile> tiles;
+    private bool waiting;
+
+
     private void Awake()
     {
         grid = GetComponentInChildren<TileGrid>();
@@ -31,25 +34,30 @@ public class TileBoard : MonoBehaviour
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) 
+        if (!waiting)
         {
-            MoveTiles(Vector2Int.up, 0, 1, 1, 1);
-        }else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            MoveTiles(Vector2Int.down, 0, 1, grid.height - 2, -1);
-        }
-        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            MoveTiles(Vector2Int.left, 1, 1, 0, 1);
-        }
-        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            MoveTiles(Vector2Int.right, grid.width - 2, -1, 0, 1);
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                MoveTiles(Vector2Int.up, 0, 1, 1, 1);
+            }
+            else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                MoveTiles(Vector2Int.down, 0, 1, grid.height - 2, -1);
+            }
+            else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                MoveTiles(Vector2Int.left, 1, 1, 0, 1);
+            }
+            else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                MoveTiles(Vector2Int.right, grid.width - 2, -1, 0, 1);
+            }
         }
     }
 
     private void MoveTiles(Vector2Int direction, int startX, int incrementX, int startY, int incrementY)
     {
+        bool changed = false;
         for(int x = startX; x >= 0 && x < grid.width; x += incrementX)
         {
             for(int y = startY; y >= 0 && y < grid.height; y += incrementY)
@@ -58,33 +66,91 @@ public class TileBoard : MonoBehaviour
 
                 if(cell.occupied)
                 {
-                    MoveTile(cell.tile, direction);
+                    changed = MoveTile(cell.tile, direction);
                 }
             }
         }
+
+        if (changed)
+        {
+            StartCoroutine(WaitForChanges());
+        }
     }
 
-    private void MoveTile(Tile tile, Vector2Int direction)
+    private bool MoveTile(Tile tile, Vector2Int direction)
     {
         TileCell newCell = null;
-        TileCell adjacent = grid.GetAdjacentCell(tile.cell, direction);   //인접한 cell
+        TileCell adjacent = grid.GetAdjacentCell(tile.cell, direction);   //해당 방향의 좌표값 반환
 
-        while(adjacent!= null) 
+        while(adjacent!= null)      //반환된 좌표에 타일이 있으면
         { 
-            if(adjacent.occupied)
+            if(adjacent.occupied)   //타일이 있으면
             {
                 //병합
+                if (CanMerge(tile, adjacent.tile))
+                {
+                    Merge(tile, adjacent.tile);
+                    return true;
+                }
                 break;
             }
 
             newCell = adjacent;
-            adjacent = grid.GetAdjacentCell(adjacent, direction);
-
+            adjacent = grid.GetAdjacentCell(adjacent, direction);       //해당 방향으로 새로운 좌표 반환
         }
-        if(newCell!= null)
+        if(newCell != null)          
         {
-            tile.MoveTo(newCell);
+            tile.MoveTo(newCell);   //새로운 칸으로 이동
+            return true;
         }
+        return false;
+    }
+
+    private bool CanMerge(Tile a, Tile b)
+    {
+        return a.number == b.number && !b.locked;    
+    }
+
+    private void Merge(Tile a, Tile b)
+    {
+        tiles.Remove(a);
+        a.Merge(b.cell);
+
+        int index = Mathf.Clamp(IndexOf(b.state) + 1,0,tileStates.Length -1 );
+        int number = b.number * 2;
+
+        b.SetState(tileStates[index],number);
+    }
+
+    public int IndexOf(TileState state)
+    {
+        for(int i = 0; i < tileStates.Length; i++) 
+        {
+            if(state == tileStates[i])
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+    private IEnumerator WaitForChanges()
+    {
+        waiting = true;
+
+        yield return new WaitForSeconds(0.1f);
+
+        waiting = false;
+
+        foreach(var tile in tiles)
+        {
+            tile.locked = false;
+        }
+
+        if(tiles.Count != grid.size)
+        {
+            CreateTile();
+        }
+        //게임오버 체크
     }
 }
 
